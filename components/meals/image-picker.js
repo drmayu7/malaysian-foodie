9205e15d31d81ec7d3755cbdde10dbdfe412f778
Dr Muhammad Naufal <117550441+drmayu7@users.noreply.github.com>
2025-03-13 22:07:53 +0800

    add image-picker components

--- components/meals/image-picker.js
+++ components/meals/image-picker.js
@@ -1,12 +1,30 @@
 'use client';
 import classes from "./image-picker.module.css";
-import { useRef } from "react";
+import {useRef, useState} from "react";
+import Image from "next/image";
 
 export default function ImagePicker({label,name}) {
+    const [pickedImage,setPickedImage] = useState();
     const imageInputRef = useRef();
 
     function handlePickClick(){
         imageInputRef.current.click();
+    }
+
+    function handleImageChange(event){
+        const file = event.target.files[0];
+        if (!file){
+            setPickedImage(null);
+            return;
+        }
+
+        const fileReader = new FileReader();
+
+        fileReader.onload = () => {
+            setPickedImage(fileReader.result);
+        };
+
+        fileReader.readAsDataURL(file);
     }
 
     return (
@@ -14,12 +32,23 @@ export default function ImagePicker({label,name}) {
             <div className={classes.picker}>
                 <label htmlFor={name} >{label}</label>
                 <div className={classes.controls}>
+                    <div className={classes.preview}>
+                        {!pickedImage && <p>No image picked yet.</p>}
+                        {pickedImage && (
+                            <Image
+                                src={pickedImage}
+                                alt='Picked Image'
+                                fill
+                            />)}
+                    </div>
                     <input className={classes.input}
                            type='file'
                            id={name}
                            accept='image/png, image/jpeg'
                            name={name}
                            ref={imageInputRef}
+                           onChange={handleImageChange}
+                           required
                     />
                     <button className={classes.button}
                             type='button'
